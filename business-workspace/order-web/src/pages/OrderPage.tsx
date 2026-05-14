f314d00eb1d32510bb286ab09960692c4043cdf4
guandezhi <guandezhi@yingzi.com>
2026-05-14 18:39:33 +0800

    day10 finish

---
+++ business-workspace/order-web/src/pages/OrderPage.tsx
@@ -0,0 +1,7 @@
+{
+    "queryContent": "订单管理系统，包含创建、查询、列表功能",
+    "modules": "订单模块",
+    "tables": "order(id,order_no,user_id,amount,status)",
+    "apis": "/order/create, /order/list, /order/get",
+    "pages": "订单列表页、订单详情页、订单创建页"
+}
